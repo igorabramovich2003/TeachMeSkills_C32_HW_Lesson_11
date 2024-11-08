8751b348fca757e7b82561fc11aab29d20a22a5a
Igor Abramovich <igorab2033@gmail.com>
2024-11-08 10:55:11 +0300

    done

--- src/com/teachmeskills/lesson_11/ApplicationRunner.java
+++ src/com/teachmeskills/lesson_11/ApplicationRunner.java
@@ -1,4 +1,28 @@
 package com.teachmeskills.lesson_11;
 
+import com.teachmeskills.lesson_11.operation.StringOperation;
+/*
+Write a program with the following functionality:
+Pass a string as input (we will assume that this is a standard document number in a given format).
+The document number has the format xxxx-yyy-xxxx-yyy-xyxy, where x is any digit, and y is a letter of the Latin alphabet (can be upper or lower case).
+*first* Display the first two blocks of 4 digits on one line.
+*second* Display the document number, but replace the blocks of three letters with *** (each letter will be replaced with *).
+*third* Display only the letters from the document number in the format yyy/yyy/y/y in lower case.
+*fourth* Display the letters from the document number in the format "Letters:yyy/yyy/y/y" in upper case (implemented using the StringBuilder class).
+*fifth* Check whether the document number contains the sequence "abc" and display a message whether it does or not (moreover, abc and ABC are considered the same sequence).
+*sixth* Check if the document number starts with the sequence 555.
+*seventh* Check if the document number ends with the sequence 1a2b.
+All these methods are implemented in a separate class in static methods, each task - in a separate method.
+ */
 public class ApplicationRunner {
+    public static void main(String[] args) {
+        String DocumentNumber = "5551-ABC-2222-Bbb-1a2b";
+        System.out.println(StringOperation.first(DocumentNumber));
+        System.out.println(StringOperation.second(DocumentNumber));
+        System.out.println(StringOperation.third(DocumentNumber));
+        System.out.println(StringOperation.fourth(DocumentNumber));
+        System.out.println(StringOperation.fifth(DocumentNumber));
+        System.out.println(StringOperation.sixth(DocumentNumber));
+        System.out.println(StringOperation.seventh(DocumentNumber));
+    }
 }
--- src/com/teachmeskills/lesson_11/operation/StringOperation.java
+++ src/com/teachmeskills/lesson_11/operation/StringOperation.java
@@ -1,4 +1,43 @@
 package com.teachmeskills.lesson_11.operation;
 
 public class StringOperation {
+    //- Display the first two blocks of 4 digits on the screen in one line.
+    public static String first(String DocumentNumber){
+        return DocumentNumber.substring(0,4) + " " + DocumentNumber.substring(9,13);
+    }
+    //- Display the document number, but replace blocks of three letters with *** (each letter will be replaced with *).
+    public static String second(String DocumentNumber){
+        DocumentNumber = DocumentNumber.replaceAll(DocumentNumber.substring(5,8),"***");
+        return DocumentNumber.replaceAll(DocumentNumber.substring(14, 17), "***");
+    }
+    //- Display only one letter from the document number in the format yyy/yyy/y/y in lowercase.
+    public static String third(String DocumentNumber){
+        String[] separator = DocumentNumber.split("-");
+        separator[4] = separator[4].replaceAll(separator[4].substring(0, 1), "/");
+        separator[4] = separator[4].replaceAll(separator[4].substring(2, 3), "/");
+        return (separator[1].concat("/") + separator[3] + separator[4]).toLowerCase();
+    }
+    //- Display the letters from the document number in the format "Letters:yyy/yyy/y/y" in uppercase (implemented using the StringBuilder class).
+    public static String fourth(String DocumentNumber){
+        StringBuilder sb = new StringBuilder(DocumentNumber);
+        sb.delete(0,5);
+        sb.replace(3, 9,"/");
+        sb.deleteCharAt(7);
+        sb.setCharAt(7, '/');
+        sb.setCharAt(9, '/');
+        sb.insert(0, "Letters:");
+        return sb.toString().toUpperCase();
+    }
+    //- Check whether the document number contains the sequence "abc" and display a message whether it does or not (and abc and ABC are considered the same sequence).
+    public static boolean fifth(String DocumentNumber){
+        return DocumentNumber.toLowerCase().contains("abc");
+    }
+    //- Check whether the document number begins with the sequence 555.
+    public static boolean sixth(String DocumentNumber){
+        return DocumentNumber.startsWith("555");
+    }
+    //- Check whether the document number ends with the sequence 1a2b.
+    public static boolean seventh(String DocumentNumber){
+        return DocumentNumber.endsWith("1a2b");
+    }
 }
